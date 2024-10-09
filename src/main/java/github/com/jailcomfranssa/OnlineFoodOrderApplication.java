18b5a44f44406d8410fc04a417a100452d43b35f
jailcomfranssa <jail_son_0@hotmail.com>
2024-10-09 16:38:07 -0300

    first commit

---
+++ src/main/java/github/com/jailcomfranssa/OnlineFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package github.com.jailcomfranssa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/jailcomfranssa/OnlineFoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.jailcomfranssa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
